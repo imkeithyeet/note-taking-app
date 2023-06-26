479f307f5c999b0781af1b2abc248e089064e478
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-06-25 23:25:00 -0700

    formatted

--- src/NoteLayout.tsx
+++ src/NoteLayout.tsx
@@ -1,18 +1,20 @@
-import { Navigate, Outlet, useOutletContext, useParams } from "react-router-dom"
-import { Note } from "./App"
+import { Navigate, Outlet, useOutletContext, useParams } from "react-router-dom";
+import { Note } from "./App";
 
 type NoteLayoutProps = {
-    notes: Note[]
+  notes: Note[];
+};
+
+export function NoteLayout({ notes }: NoteLayoutProps) {
+  const { id } = useParams();
+  const note = notes.find((n) => n.id === id);
+
+  if (note == null) {
+    return <Navigate to="/" replace />;
+  }
+  return <Outlet context={note} />;
 }
 
-export  function NoteLayout({ notes }: NoteLayoutProps){
-    const {id} = useParams()
-    const note = notes.find(n => n.id === id)
-
-    if(note == null) return <Navigate to="/" replace />
-    return <Outlet context={note} />
-
-}
-export function useNote(){
-    return useOutletContext<Note>()
+export function useNote() {
+  return useOutletContext<Note>();
 }
